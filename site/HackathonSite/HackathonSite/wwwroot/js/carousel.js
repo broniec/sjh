2d98040d482969e00faf6b0a2fe77ab6dc18149e
Will Broniec <williambroniec@gmail.com>
2020-11-04 20:02:49 -0500

    a couple of minor changes were necessary
    it's working now though, just need a home page lol

--- site/HackathonSite/HackathonSite/wwwroot/js/carousel.js
+++ site/HackathonSite/HackathonSite/wwwroot/js/carousel.js
@@ -1,40 +1,42 @@
 ﻿var reference;
 
 function initCarousel() {
-    // add active class
-    $('#link-diversity').addClass('active');
-    // create the grid
-    $(".h-carousel").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3
-    });
-    // register change event
-    $('.h-carousel').on('afterChange', function (_event, _slick, currentSlide) {
-        // remove active class
-        $('#link-diversity').removeClass('active');
-        $('#link-erg').removeClass('active');
-        $('#link-events').removeClass('active');
-        $('#link-salary').removeClass('active');
+    if ($('.h-carousel').not('.slick-initialized').length) {
+        // add active class
+        $('#link-diversity').addClass('active');
+        // create the grid
+        $(".h-carousel").slick({
+            dots: true,
+            infinite: true,
+            centerMode: true,
+            slidesToShow: 3
+        });
+        // register change event
+        $('.h-carousel').on('afterChange', function (_event, _slick, currentSlide) {
+            // remove active class
+            $('#link-diversity').removeClass('active');
+            $('#link-erg').removeClass('active');
+            $('#link-events').removeClass('active');
+            $('#link-salary').removeClass('active');
 
-        if (currentSlide === 0) {
-            console.log('diversity score');
-            $('#link-diversity').addClass('active');
-        } else if (currentSlide === 1) {
-            console.log('erg');
-            $('#link-erg').addClass('active');
-        } else if (currentSlide === 2) {
-            console.log('d&i events');
-            $('#link-events').addClass('active');
-        } else if (currentSlide === 3) {
-            console.log('salaries');
-            $('#link-salary').addClass('active');
-        }
+            if (currentSlide === 0) {
+                console.log('diversity score');
+                $('#link-diversity').addClass('active');
+            } else if (currentSlide === 1) {
+                console.log('erg');
+                $('#link-erg').addClass('active');
+            } else if (currentSlide === 2) {
+                console.log('d&i events');
+                $('#link-events').addClass('active');
+            } else if (currentSlide === 3) {
+                console.log('salaries');
+                $('#link-salary').addClass('active');
+            }
 
-        // todo - somehow call .net back
-        reference.invokeMethodAsync('UpdateContent', currentSlide);
-    });
+            // todo - somehow call .net back
+            reference.invokeMethodAsync('UpdateContent', currentSlide);
+        });
+    }
 }
 
 function cacheReference(dotNetReference) {
