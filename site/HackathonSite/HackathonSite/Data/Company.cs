69e0ff19d207bb2c1a1147e4fd0051ebbc7632bb
Will Broniec <williambroniec@gmail.com>
2020-11-13 20:12:36 -0500

    get google calendar integration working

--- site/HackathonSite/HackathonSite/Data/Company.cs
+++ site/HackathonSite/HackathonSite/Data/Company.cs
@@ -7,5 +7,9 @@
         public string Location { get; set; }
         // maybe we ditch this...
         public string LogoLink { get; set; }
+
+        public string CalendarLink { get; set; }
+        public string DiversityText { get; set; }
+        public string ERGText { get; set; }
     }
 }
